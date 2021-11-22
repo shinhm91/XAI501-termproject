f237da10a0471457d79cd1a9873c1760f137e556
shinhm91 <93113221+shinhm91@users.noreply.github.com>
2021-11-22 21:33:24 +0900

    Update elliptic_envelope.py

--- xai/models/elliptic_envelope.py
+++ xai/models/elliptic_envelope.py
@@ -7,8 +7,7 @@ Created on Tue Nov 16 16:09:16 2021
 """
 import numpy as np
 from sklearn.covariance import EllipticEnvelope
-from sklearn.decomposition import PCA, LatentDirichletAllocation
-from sklearn.pipeline import Pipeline
+#from sklearn.pipeline import Pipeline
 from sklearn.metrics import roc_auc_score,make_scorer
 from sklearn.model_selection import StratifiedKFold, GridSearchCV
 import umap # pip install umap-learn
@@ -36,31 +35,24 @@ class ellenvelope():
         # Grid Search over contamination fraction
         skf = StratifiedKFold(n_splits=5)
         folds = list(skf.split(trainX, trainy))
-        pipe = Pipeline([
-            ("pca", PCA(random_state=777)),
-            ("lda", LatentDirichletAllocation()),
-            ("umap", umap(random_state=456)),
-            ("clf",EllipticEnvelope())
-            ])
-        
-        grid_params = [{'pca__n_components':[0.8, 2, 4,8],'pca__svd_solver':['full','arpack','randomized'],
-                        'lda':['passthrough'],'umap':['passthrough'],
-                        'clf__contamination':np.linspace(0.0, 0.05, 5,15)},
-                       {'lda__n_components': [10, 15, 20, 25, 30], 'lda__learning_decay': [.5, .7, .9],
-                         'pca':['passthrough'],'umap':['passthrough'],
-                        'clf__contamination':np.linspace(0.0, 0.05, 5,15)},
-                       {'umap__n_neighbors':[0.3,3,6,12,20],'umap__n_components':[0.8,2,4,8],
-                        'pca':['passthrough'],'lda':['passthrough'],
-                        'clf__contamination':np.linspace(0.0, 0.05, 5,15)}]
+        clf = EllipticEnvelope()        
+        clf_params = {'contamination':np.linspace(0.0, 0.05, 5,15)}
+            
         rocaucsc = make_scorer(roc_auc_score)
-        search = GridSearchCV(pipe, grid_params, scoring=rocaucsc, cv=folds)
+        search = GridSearchCV(clf, clf_params, scoring=rocaucsc, cv=folds)
         search.fit(trainX, trainy)
         
         self.optimal = search.best_estimator_
         self.learned = True
         
         return self
-        
+    
+    def get_cv_rocauc(self):
+        return search.cv_results_['mean_test_score']
+    
+    def get_gridcv_params(self):
+        return search.cv_results_['params']
+           
     def predict(self, testX):
         """
         
